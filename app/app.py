4a860268274d037aeb979475b3d2fb18b7a4b87d
ilya <utkin.ilya.nickolaevich@gmail.com>
2022-05-02 13:21:17 +0300

    templates added

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import sqlite3
 from typing import Optional, List, Any
 
 import spacy
-from flask import Flask, g, request
+from flask import Flask, g, request, render_template
 
 app = Flask(__name__)
 MAIN_DATABASE = 'ted_quotes.db'
@@ -19,20 +19,21 @@ def get_db() -> sqlite3.Connection:
 
 @app.route('/')
 def index():
-    cur = get_db().cursor()
     query = request.args.get('query')
     if query is None:
-        return "Please provide query"
+        return render_template('welcome.html')
+
     offset = request.args.get('offset')
     if offset is None:
         offset = 0
     limit = request.args.get('limit')
     if limit is None:
         limit = 5
-    links = get_link(cur, query, limit, offset)
-    clickable = [f'<a href="{link}">{link}</a>' for link in links]
-    return '<br>'.join(clickable)
 
+    cur = get_db().cursor()
+    links = get_link(cur, query, limit, offset)
+    cur.close()
+    return render_template('search_result.html', links=links)
 
 
 def get_link(cursor: sqlite3.Cursor, query: str, limit: int, offset: int) -> Optional[List[Any]]:
@@ -43,22 +44,25 @@ def get_link(cursor: sqlite3.Cursor, query: str, limit: int, offset: int) -> Opt
     word = words[0]
     cursor.execute(
         """
-        select link, start_time, end_time from words
+        select link, quote, start_time, end_time from words
         join word_to_quote on words.ROWID = word_id
         join subtitles on word_to_quote.quote_id=subtitles.ROWID
         join video_to_quote on video_to_quote.quote_id=word_to_quote.quote_id
         join videos on videos.ROWID = video_id
         WHERE word = ? limit ? offset ?;
         """
-    , [word, limit, offset])
+        , [word, limit, offset]
+    )
     results = cursor.fetchall()
     links = []
+    quotes = []
     for r in results:
-        link, start_time, end_time = r
-        start_shifted = max(0, int(start_time) // 1000 - 5)
-        end_shifted = int(end_time) // 1000 + 10
+        link, quote, start_time, end_time = r
+        start_shifted = max(0, int(start_time) // 1000 - 3)
+        end_shifted = int(end_time) // 1000 + 5
         links.append(link + f'#t={start_shifted},{end_shifted}')
-    return links
+        quotes.append(quote)
+    return list(zip(links, quotes))
 
 
 if __name__ == '__main__':
